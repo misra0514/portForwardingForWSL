0013497908c8c3046fecaa2dd62af5fdc60a59d3
misra0514 <51090056+misra0514@users.noreply.github.com>
2021-03-26 11:17:21 +0800

    Add files via upload

---
+++ setWsl.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+:: MADE BY MISRA0514
+:: MORE INFORMATION PLZ VIEW https://github.com/misra0514/portForwardingForWSL/
+SET LXDISTRO=Ubuntu & SET WSL2PORT=8080 & SET HOSTPORT=8001
+NETSH INTERFACE PORTPROXY RESET & NETSH AdvFirewall Firewall delete rule name="%LXDISTRO% Port Forward" > NUL
+WSL -d %LXDISTRO% -- ip addr show eth0 ^| grep -oP '(?^<=inet\s)\d+(\.\d+){3}' > IP.TMP
+SET /p IP=<IP.TMP
+NETSH INTERFACE PORTPROXY ADD v4tov4 listenport=%HOSTPORT% listenaddress=0.0.0.0 connectport=%WSL2PORT% connectaddress=%IP% 
+NETSH AdvFirewall Firewall add rule name="%LXDISTRO% Port Forward" dir=in action=allow protocol=TCP localport=%HOSTPORT% > NUL
+ECHO WSL2 Virtual Machine %IP%:%WSL2PORT%now accepting traffic on %COMPUTERNAME%:%HOSTPORT%
+
+ECHO success!
+pause
