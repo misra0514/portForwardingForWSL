97d5dc89a918fd588231595f2218bb5bc56e2394
guoyufeng on desktop01 <1242383961@qq.com>
2021-05-31 15:32:24 +0800

    feat: run as admin automatically

--- setWsl.bat
+++ setWsl.bat
@@ -1,6 +1,21 @@
 @ECHO OFF
 :: MADE BY MISRA0514
 :: MORE INFORMATION PLZ VIEW https://github.com/misra0514/portForwardingForWSL/
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+
 SET LXDISTRO=Ubuntu & SET WSL2PORT=8080 & SET HOSTPORT=8001
 NETSH INTERFACE PORTPROXY RESET & NETSH AdvFirewall Firewall delete rule name="%LXDISTRO% Port Forward" > NUL
 WSL -d %LXDISTRO% -- ip addr show eth0 ^| grep -oP '(?^<=inet\s)\d+(\.\d+){3}' > IP.TMP
@@ -9,5 +24,5 @@ NETSH INTERFACE PORTPROXY ADD v4tov4 listenport=%HOSTPORT% listenaddress=0.0.0.0
 NETSH AdvFirewall Firewall add rule name="%LXDISTRO% Port Forward" dir=in action=allow protocol=TCP localport=%HOSTPORT% > NUL
 ECHO WSL2 Virtual Machine %IP%:%WSL2PORT%now accepting traffic on %COMPUTERNAME%:%HOSTPORT%
 
-ECHO success!
+ECHO config success!
 pause
